e4061d1c9ae366968b43e47bd90d9e84d2345899
valdmel <valmneves92@gmail.com>
2021-08-07 09:38:24 -0300

    Add bomb scriptable object

--- Assets/Scripts/Gameplay/Bomb.cs
+++ Assets/Scripts/Gameplay/Bomb.cs
@@ -7,14 +7,22 @@ public class Bomb : MonoBehaviour
 
     #region SERIALIZABLE
     [Header("Bomb Properties")]
-    [SerializeField] private float moveSpeed = 1f;
+    [SerializeField] private BombData bombData;
     #endregion
 
+    private float moveSpeed;
+    private Vector3 moveDirection;
     private Rigidbody rigidbody;
     #endregion
 
     #region MONOBEHAVIOUR CALLBACK METHODS
     private void Awake() => rigidbody = GetComponent<Rigidbody>();
+
+    private void Start()
+    {
+        moveSpeed = bombData.MoveSpeed;
+        moveDirection = bombData.MoveDirection;
+    }
 
     private void FixedUpdate() => AddVelocity();
 
@@ -30,7 +38,7 @@ public class Bomb : MonoBehaviour
     #endregion
 
     #region CLASS METHODS
-    public void AddVelocity() => transform.Translate(new Vector3(moveSpeed, Vector3.zero.y, Vector3.zero.z));
+    public void AddVelocity() => transform.Translate(new Vector3(moveDirection.x * moveSpeed, Vector3.zero.y, moveDirection.z * moveSpeed));
 
     private void InvertMoveSpeed() => moveSpeed = -moveSpeed;
     #endregion
