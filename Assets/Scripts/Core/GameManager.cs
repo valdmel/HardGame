03aa04e95115caf19dcdc86ce66198d1be4f9d63
valdmel <valmneves92@gmail.com>
2021-08-12 20:12:30 -0300

    Add more elements to the UI

--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -10,10 +10,18 @@ public class GameManager : Singleton<GameManager>
     private const int MAX_MINUTES = 60;
     private const int STARTING_TIME = 0;
 
+    public static Action<string> OnDeathCounterChange;
     public static Action<string> OnTimeChange;
 
+    private int deathCounter = 0;
     private int seconds;
     private Coroutine timerCoroutine;
+    #endregion
+
+    #region MONOBEHAVIOUR CALLBACK METHODS
+    private void OnEnable() => PlayerBody.OnPlayerDeath += UpdateDeathCounter;
+
+    private void OnDisable() => PlayerBody.OnPlayerDeath -= UpdateDeathCounter;
     #endregion
 
     #region CLASS METHODS
@@ -44,6 +52,13 @@ public class GameManager : Singleton<GameManager>
         }
 
         StopTimer();
+    }
+
+    private void UpdateDeathCounter()
+    {
+        ++deathCounter;
+
+        OnDeathCounterChange?.Invoke(deathCounter.ToString());
     }
 
     private string ConvertSeconds(int seconds) => TimeSpan.FromSeconds(seconds).ToString(TIMESPAN_PATTERN);
--- Assets/Scripts/UI/HUD.cs
+++ Assets/Scripts/UI/HUD.cs
@@ -1,25 +1,33 @@
-using System;
 using TMPro;
 using UnityEngine;
 
 public class HUD : MonoBehaviour
 {
     #region VARIABLES
-    private const string TIME_TEXT_PATTERN = @"hh\:mm\:ss";
-
     #region SERIALIZABLE
     [Header("HUD Properties")]
+    [SerializeField] private TMP_Text deathCounterText;
     [SerializeField] private TMP_Text timerText;
     #endregion
     #endregion
 
     #region MONOBEHAVIOUR CALLBACK METHODS
-    private void OnEnable() => GameManager.OnTimeChange += DisplayTime;
+    private void OnEnable()
+    {
+        GameManager.OnDeathCounterChange += DisplayDeathCounter;
+        GameManager.OnTimeChange += DisplayTime;
+    }
 
-    private void OnDisable() => GameManager.OnTimeChange -= DisplayTime;
+    private void OnDisable()
+    {
+        GameManager.OnDeathCounterChange -= DisplayDeathCounter;
+        GameManager.OnTimeChange -= DisplayTime;
+    }
     #endregion
 
     #region CLASS METHODS
-    public void DisplayTime(string timeString) => timerText.text = timeString;
+    public void DisplayDeathCounter(string deathCounter) => deathCounterText.text = deathCounter;
+
+    public void DisplayTime(string time) => timerText.text = time;
     #endregion
 }
