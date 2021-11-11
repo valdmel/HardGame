649a82ba5152e39a0734763beee4e1cabd029de6
valdmel <valmneves92@gmail.com>
2021-11-11 20:13:53 -0300

    Refactor how door buttons work

--- Assets/Scripts/Gameplay/Environment/Level/DoorButton.cs
+++ Assets/Scripts/Gameplay/Environment/Level/DoorButton.cs
@@ -3,15 +3,21 @@ using UnityEngine;
 public class DoorButton : MonoBehaviour
 {
     #region VARIABLES
+    private const string PRESS_TRIGGER = "Press";
+    private const string RELEASE_TRIGGER = "Release";
+
     #region SERIALIZABLE
     [Header("Door Button Properties")]
     [SerializeField] private GameObject doorBodyObjectToOpen;
     #endregion
 
+    private Animator animator;
     private bool isPressed = false;
     #endregion
 
     #region MONOBEHAVIOUR CALLBACK METHODS
+    private void Awake() => animator = GetComponent<Animator>();
+
     private void OnEnable() => PlayerBody.onPlayerTouchSuperBomb += ReleaseButton;
 
     private void OnDisable() => PlayerBody.onPlayerTouchSuperBomb -= ReleaseButton;
@@ -27,8 +33,16 @@ public class DoorButton : MonoBehaviour
     #endregion
 
     #region CLASS METHODS
-    private void PressButton() => isPressed = true;
+    private void PressButton()
+    {
+        isPressed = true;
+        animator.SetTrigger(PRESS_TRIGGER);
+    }
 
-    private void ReleaseButton() => isPressed = false;
+    private void ReleaseButton()
+    {
+        isPressed = false;
+        animator.SetTrigger(RELEASE_TRIGGER);
+    }
     #endregion
 }
