ad3da3f6c6920ddf63d4fe4c976713a954e853dd
valdmel <valmneves92@gmail.com>
2021-11-30 21:40:56 -0300

    Pause the game BGM when pausing the game and resume it when unpausing

--- Assets/Scripts/Core/Managers/MusicManager.cs
+++ Assets/Scripts/Core/Managers/MusicManager.cs
@@ -1,4 +1,35 @@
+using UnityEngine;
+
 public class MusicManager : Singleton<MusicManager>
 {
+    #region VARIABLES
+    private AudioSource audioSource;
+    #endregion
 
+    #region MONOBEHAVIOUR CALLBACK METHODS
+    protected override void Awake()
+    {
+        base.Awake();
+
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnEnable() => GameManager.onGamePause += HandleAudio;
+
+    private void OnDisable() => GameManager.onGamePause -= HandleAudio;
+    #endregion
+
+    #region CLASS METHODS
+    private void HandleAudio()
+    {
+        if (audioSource.isPlaying)
+        {
+            audioSource.Pause();
+
+            return;
+        }
+
+        audioSource.Play();
+    }
+    #endregion
 }
